ecd037ccd62bc198343efbb4706c5626b3ee9823
Denis <zaq.den@gmail.com>
2024-06-10 09:32:42 +0400

    Update module_2_5.py

--- module_2_5.py
+++ module_2_5.py
@@ -4,9 +4,12 @@ def get_matrix (n, m, value):   # Объявление функции
         matrix.append([])   # добавление строк из переменной n
         for j in range(m):  # цикл для кол-во столбцов
             matrix[i].append(value) # заполняет значение value
-    print(matrix)
+    return matrix
 
+matrix1 = get_matrix(2, 2, 10)
+matrix2 = get_matrix(3, 5, 42)
+matrix3 = get_matrix(4, 2, 13)
 
-get_matrix(2,2,10)
-get_matrix(3,5,42)
-get_matrix(4,2,13)
+print(matrix1)
+print(matrix2)
+print(matrix3)
